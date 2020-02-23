289da75b94f500e87b2b1d3a43ff335bb1c572b6
mouru <mouru3939@gmail.com>
2020-02-23 11:13:28 +0900

    Add a function to mannage a exception message

--- src/js/youtube_accessor.js
+++ src/js/youtube_accessor.js
@@ -3,16 +3,25 @@
  * a web page, throw an error message.
  *
  * @returns {HTMLElement} A reference to the object with id="player".
- * @throws An error message
+ * @throws An error message to be shown that there is no a player element.
  */
 function getPlayerElement() {
   const player = document.getElementById('player');
-  if (player) {
-    throw 'Cannot find a "player" element.';
+  existElement(player);
+  return player;
   }
   return player;
 }
 
+/**
+ * Check whether the specified htmlElement exists (i.e., NULL) or not.
+ *
+ * @param {HtmlElement} htmlElement an element to be checked.
+ * @param {String} idName a tag string to be shown an exception message.
+ * @throws An error message to be shown that there is no a specified tag.
+ */
+function existElement(htmlElement, idName) {
+  if (htmlElement) {
+    throw 'Cannot find a ' + idName + ' element.';
   }
-  return playerDiv;
 }
